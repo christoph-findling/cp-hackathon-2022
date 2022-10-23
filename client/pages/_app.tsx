720e26399b1a0f5f83a682308d07d5f69c2e8a52
Christoph Findling <kukuruz@tuta.io>
2022-10-23 22:37:01 +0300

    wip

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,17 +1,76 @@
 import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+
 import type { AppProps } from 'next/app'
-import {  useStore } from 'react-redux'
+import { useStore } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
 import { wrapper } from '../store/store';
 
-function MyApp({ Component, pageProps }: AppProps) {
-const store = useStore();
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, chain, createClient, WagmiConfig, Chain } from 'wagmi';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 
-return (
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+  const localChain: Chain = {
+    id: 137,
+    name: 'LocalPolygonChain',
+    network: 'localhost 8545',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Ethereum',
+      symbol: 'ETH',
+    },
+    rpcUrls: {
+      default: 'http://127.0.0.1:8545',
+    },
+    // blockExplorers: {
+    //   default: { name: 'SnowTrace', url: 'https://snowtrace.io' },
+    // },
+    testnet: true,
+  }
+
+  // const { chains, provider } = configureChains(
+  const { chains, provider } = configureChains(
+    [localChain],
+    // [ alchemyProvider({ apiKey: 'dDEHfJFvReWVH3JNKtyzSi2B6UX_fWVG' })    ]
+    [ 
+      jsonRpcProvider({
+        rpc: (chain) => {
+          if (chain.id !== localChain.id) return null
+          return { http: chain.rpcUrls.default }
+        },
+      }),
+     ]
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    chains
+  });
+
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+  })
+
+
+  const store = useStore();
+
+  return (
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     <PersistGate loading={null} persistor={store._persist} >
-      <Component {...pageProps}></Component>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <Component {...pageProps}></Component>
+        </RainbowKitProvider>
+      </WagmiConfig>
     </PersistGate>
   )
 }
--- client/pages/portfolio.tsx
+++ client/pages/portfolio.tsx
@@ -1,30 +1,40 @@
 import { NextPage } from 'next';
 import { useSelector } from 'react-redux';
+import CustomButton from '../components/button';
 import Layout from '../components/layout';
 import CustomLink from '../components/link';
 import AssetSelector from '../components/slider';
 import { selectResultState } from '../store/resultSlice';
 
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import {BasketDemoSdk} from '../tslib/basket-demo-sdk';
+
+
 const Portfolio: NextPage = () => {
     const resultState = useSelector(selectResultState);
     console.log('result state: ', resultState)
 
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    BasketDemoSdk.init();
+
     return (
         <Layout>
-                <div className="w-full flex items-center justify-center">
-                    <div className='w-3/5'>
-                        <div>
-                            <h1 className="text-lg italic mb-2">Recommended asset distribution, based on your answers:</h1>
-                        </div>
-                        <div className="p-5 mb-5 border rounded-lg shadow-lg shadow-slate-100">
-                            <AssetSelector input={resultState} />
-                        </div>
-                        <div className="w-full flex justify-between items-center">
-                                <CustomLink href="/advisor" title="Back to advisor" type="button" />
-                                <CustomLink href="/" title="Continue" type="button" />
-                        </div>
+            <div className="w-full flex items-center justify-center">
+                <div className='w-3/5'>
+                    <div>
+                        <h1 className="text-lg italic mb-2">Recommended asset distribution, based on your answers:</h1>
+                    </div>
+                    <div className="p-5 mb-5 border rounded-lg shadow-lg shadow-slate-100">
+                        <AssetSelector input={resultState} />
+                    </div>
+                    <div className="w-full flex justify-between items-center">
+                        <CustomLink href="/advisor" title="Back to advisor" type="button" />
+                        <ConnectButton />                               
+                        <a><CustomButton title="Init" /></a>
                     </div>
                 </div>
+            </div>
         </Layout>
     )
 }
