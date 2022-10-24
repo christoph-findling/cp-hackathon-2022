6fc3b761f76eabe3d8bcfc75895bf7d1bcdaecda
Christoph Findling <kukuruz@tuta.io>
2022-10-24 11:34:11 +0300

    wip: integrates demo sdk

--- client/pages/portfolio.tsx
+++ client/pages/portfolio.tsx
@@ -8,15 +8,24 @@ import { selectResultState } from '../store/resultSlice';
 
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import {BasketDemoSdk} from '../tslib/basket-demo-sdk';
+import { useSigner } from 'wagmi';
+import { Signer } from 'ethers';
 
 
 const Portfolio: NextPage = () => {
     const resultState = useSelector(selectResultState);
     console.log('result state: ', resultState)
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    BasketDemoSdk.init();
+    const { data: signer, isError, isLoading } = useSigner()
+
+    const doThing = async () => {
+        const basket = new BasketDemoSdk();
+        basket.init(signer as Signer);
+        console.log('val ', basket.defaultBasketBlueprintName);
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        console.log(JSON.stringify(await basket.getSpendAmounts('0xfCA7f990115e4Bd6134Db9D5A32885C9c362f677', 33 * 1e6, 1000000000)));
+    }
 
     return (
         <Layout>
@@ -31,7 +40,7 @@ const Portfolio: NextPage = () => {
                     <div className="w-full flex justify-between items-center">
                         <CustomLink href="/advisor" title="Back to advisor" type="button" />
                         <ConnectButton />                               
-                        <a><CustomButton title="Init" /></a>
+                        <a onClick={() => doThing()}><CustomButton title="Init" /></a>
                     </div>
                 </div>
             </div>
