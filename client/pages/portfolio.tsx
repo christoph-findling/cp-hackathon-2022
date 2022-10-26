7f67039df07a16488c3de582760be81e3573d354
Christoph Findling <kukuruz@tuta.io>
2022-10-26 13:36:19 +0300

    feature: prepares asset amount fetch

--- client/pages/portfolio.tsx
+++ client/pages/portfolio.tsx
@@ -21,10 +21,13 @@ const loadingSvg = `<div style="display: flex; justify-content: center; align-it
 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
 </svg></div>`
 
+let basketSdk: any = null;
+
 const Portfolio: NextPage = () => {
 	const resultState: Result = useSelector(selectResultState)
 	const [initState, setInitState] = useState(false)
 	const [modalOpenState, setModalOpenState] = useState(false)
+	const [assetAmounts, setAssetAmounts] = useState([]);
 	const prevResultRef = useRef()
 	const dispatch = useDispatch()
 	const { data: signer, isError, isLoading } = useSigner()
@@ -34,7 +37,7 @@ const Portfolio: NextPage = () => {
 			return
 		}
 		setInitState(true)
-		const basketSdk = new BasketDemoSdk()
+		basketSdk = new BasketDemoSdk()
 		basketSdk.init(signer as Signer)
 		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		// @ts-ignore
@@ -82,8 +85,6 @@ const Portfolio: NextPage = () => {
 
 	const createBasket = async () => {
 		setModalOpenState(true)
-		const basketSdk = new BasketDemoSdk()
-		basketSdk.init(signer as Signer)
 		const inputAmount = 1000
 		const result = await basketSdk.swapAndBuild(
 			resultState.svg,
@@ -125,6 +126,11 @@ const Portfolio: NextPage = () => {
 		setModalOpenState(false)
 		const confettiService = new ConfettiService()
 		confettiService.confettiCannon()
+	}
+
+	const getAssetAmounts = async () => {
+		const assetAmounts = await basketSdk.getBasketAssetAmounts(contractAddresses.basketManager, resultState?.token);
+		setAssetAmounts(assetAmounts);
 	}
 
 	return (
@@ -207,6 +213,16 @@ const Portfolio: NextPage = () => {
 										<span className='text-xl'>Token:</span>
 										<br />
 										<span className='text-lg'>{resultState?.token}</span>
+									</div>
+									{assetAmounts && (
+										assetAmounts.map((asset) => (
+											<div key={`${asset.name}_${asset.amount}`}>
+												<span>{asset.name}: {asset.amount}</span>
+											</div>
+										))
+									)}
+									<div className="flex items-center justify-center">
+										<CustomButton onClick={() => getAssetAmounts()} title="Get asset amounts" />
 									</div>
 								</div>
 							</div>
